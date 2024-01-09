89b9746e2d9846f275b67c72f15df2405eaac042
MKainouch <kainouch.med@gmail.com>
2024-01-09 15:29:44 -0800

    adding 0-memset.c file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _memset - fills memory with a constant byte,
  * @s: memory area.
@@ -10,6 +10,6 @@ char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 for (i = 0; i < n; i++)
-*(s + i) =  b;
+*[ i] =  b;
 return (s);
 }
