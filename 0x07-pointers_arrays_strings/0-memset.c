03c846752321f3d5f12577131c61d12cee120621
MKainouch <kainouch.med@gmail.com>
2024-01-09 18:47:35 -0800

    add some files

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+	int i = 0;
 
-for (i = 0; n > 0; i++)
-{
-s[i] = b;
-}
-
-return (s);
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
